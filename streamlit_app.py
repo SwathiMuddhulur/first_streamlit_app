ebfeabf0daa602368abef36ba39146fd01b9b5e7
SwathiMuddhulur <111336136+SwathiMuddhulur@users.noreply.github.com>
2022-08-16 11:16:16 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 streamlit.title("My first Streamlit")
-streamlit.header("Application")
-streamlit.text("Text")
+streamlit.header(" 🥣 Application")
+streamlit.text("🐔 Text1")
+streamlit.text("🐔 Text2")
+streamlit.text("🐔 Text3")
+streamlit.header("🍞 Second header")
