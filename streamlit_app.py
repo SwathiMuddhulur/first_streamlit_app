7e6ec88f689f8c478dcddc1295f2739baa81feb6
SwathiMuddhulur <111336136+SwathiMuddhulur@users.noreply.github.com>
2022-08-16 11:20:19 +0530

    Update streamlit_app.py
    imported pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title("My first Streamlit")
 streamlit.header(" 🥣 Application")
 streamlit.text("🐔 Text1")
 streamlit.text("🐔 Text2")
 streamlit.text("🐔 Text3")
 streamlit.header("🍞 Second header")
+my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
